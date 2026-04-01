7c8ecf4651d4d8f1351d20b9fb5090ebb8d19734
Alisa-Dariana Roman <alisa.roman@analog.com>
2026-04-01 17:28:01 +0300

    workshops: add jupiter sdr

---
+++ stages/07.extra-tweaks/01.extra-scripts/extra-jupiter-sdr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# kuiper2.0 - Embedded Linux for Analog Devices Products
+#
+# Copyright (c) 2026 Analog Devices, Inc.
+# Author: Alisa-Dariana Roman <alisa.roman@analog.com>
+
+mkdir -p /home/analog
+
+cp -r stages/07.extra-tweaks/01.extra-scripts/jupiter_profiles /home/analog
